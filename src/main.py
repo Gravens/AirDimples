bd1604cca67c79ef819d5f67dfd28686e296d695
Dmitry Semenov <lonelyteapot@gmail.com>
2021-07-07 11:57:55 +0300

    Implement moving cursor with index finger

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
 import finger_detection
+import pyautogui
+import keyboard
+
+
+def move_cursor_on_screen(normalized_finger_coords):
+    width, height = pyautogui.size()
+
+    # Convert normalized coordinates to correspond to the screen
+    screen_finger_coords = {}
+    for finger, coords in normalized_finger_coords.items():
+        screen_finger_coords[finger] = (
+            None if coords is None
+            else finger_detection.normalized_to_pixel_coordinates(coords.x, coords.y, width, height)
+        )
+    print(screen_finger_coords)
+
+    # Failsafe
+    if keyboard.is_pressed('ctrl'):
+        return
+
+    # Move the cursor
+    pyautogui.moveTo(screen_finger_coords['index'])
 
 
 if __name__ == '__main__':
+    pyautogui.FAILSAFE = False
     finger_detection.launch_detection_on_webcam()
