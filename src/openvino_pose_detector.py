febe8dfaf4b09b10710fddb7e4af24eb1e4be4cd
Arapov Denis <ArapovZWERR@yandex.ru>
2021-07-10 14:10:47 +0300

    Image and net input resizing updated

--- src/openvino_pose_detector.py
+++ src/openvino_pose_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from time import perf_counter
+from math import floor
 from pose_utils.pipelines import get_user_config, AsyncPipeline
 from pose_utils import models
 from openvino.inference_engine import IECore
@@ -11,22 +12,34 @@ def launch_detection_on_capture(capture, args):
     ie = IECore()
     start_time = perf_counter()
 
+    # prepare model params
     ret, frame = capture.read()
+
+    aspect_ratio = frame.shape[1] / frame.shape[0]
+    if aspect_ratio >= 1:
+        target_size = floor(frame.shape[0] * args["net_input_width"] / frame.shape[1])
+    else:
+        target_size = args["net_input_width"]
+
     if not ret:
         raise IOError('Can not read frame!')
 
-    frame = cv2.resize(frame, (args["net_input_size"], args["net_input_size"]), interpolation=cv2.INTER_AREA)
-
     model = models.HpeAssociativeEmbedding(ie, args["model_path"],
-                                           aspect_ratio=frame.shape[1] / frame.shape[0],
-                                           target_size=args["net_input_size"], prob_threshold=0.1)
+                                           aspect_ratio=aspect_ratio,
+                                           target_size=target_size, prob_threshold=0.1)
+
+    # cv2.resize() takes (width, height) as new size,
+    # but img.shape has (height, width) format
+    net_input_size = (model.w, model.h)
+    frame = cv2.resize(frame, net_input_size, interpolation=cv2.INTER_AREA)
+
+    show_frame_size = (1080, floor(frame.shape[0] * 1080 / frame.shape[1])) # width height
 
     hpe_pipeline = AsyncPipeline(ie, model, plugin_config, device=args["device"], max_num_requests=0)
 
     hpe_pipeline.submit_data(frame, 0, {'frame': frame, 'start_time': start_time})
     next_frame_id = 1
     next_frame_id_to_show = 0
-
 
     while True:
         if hpe_pipeline.callback_exceptions:
@@ -37,7 +50,6 @@ def launch_detection_on_capture(capture, args):
         if results:
             (poses, scores), frame_meta = results
             frame = frame_meta['frame']
-            frame = cv2.resize(frame, (args["net_input_size"], args["net_input_size"]), interpolation=cv2.INTER_AREA)
             if len(poses) > 0:
                 for pose in poses:
                     points = pose[:, :2].astype(np.int32)
@@ -47,7 +59,7 @@ def launch_detection_on_capture(capture, args):
 
             start_time = frame_meta['start_time']
             next_frame_id_to_show += 1
-            cv2.imshow("rwer", cv2.resize(frame, (1080, 1080), interpolation=cv2.INTER_AREA))
+            cv2.imshow("Show", cv2.resize(frame, show_frame_size, interpolation=cv2.INTER_AREA))
             continue
 
         if hpe_pipeline.is_ready():
@@ -56,7 +68,7 @@ def launch_detection_on_capture(capture, args):
             ret, frame = capture.read()
             if not ret:
                 break
-            frame = cv2.resize(frame, (args["net_input_size"], args["net_input_size"]), interpolation=cv2.INTER_AREA)
+            frame = cv2.resize(frame, net_input_size, interpolation=cv2.INTER_AREA)
             if frame is None:
                 break
 
@@ -84,7 +96,7 @@ def launch_detection_on_webcam(args):
 
 
 if __name__ == "__main__":
-    launch_detection_on_webcam({"cap_source": 0,
+    launch_detection_on_webcam({"cap_source": "people-detection.mp4",
                                 "model_path": "pose_utils/human-pose-estimation-0007.xml",
                                 "device": "CPU",
-                                "net_input_size": 256})
+                                "net_input_width": 516})
