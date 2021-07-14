a6825c464c4cd992a429bee52a16f21b5139117c
Dmitry Semenov <lonelyteapot@gmail.com>
2021-07-14 10:39:29 +0300

    Fix styling
    Reduce line length to under 120 characters
    Enforce exactly 1 blank line at the end of every file

--- src/finger_main.py
+++ src/finger_main.py
@@ -9,7 +9,9 @@ def move_cursor_on_screen(normalized_finger_coords):
     # Convert normalized coordinates to correspond to the screen
     screen_finger_coords = {}
     for finger, coords in normalized_finger_coords.items():
-        screen_finger_coords[finger] = finger_detection.normalized_to_pixel_coordinates((coords.x, coords.y), window_size)
+        screen_finger_coords[finger] = finger_detection.normalized_to_pixel_coordinates(
+            (coords.x, coords.y), window_size
+        )
 
     print(screen_finger_coords)
 
@@ -24,4 +26,3 @@ def move_cursor_on_screen(normalized_finger_coords):
 if __name__ == '__main__':
     pyautogui.FAILSAFE = False
     finger_detection.launch_detection_on_webcam()
-
