0a62e109cd2de0be6fb164125adb0f51b99ad524
Dmitry Semenov <lonelyteapot@gmail.com>
2021-07-19 05:19:47 +0300

    Add Intel pose model constants

--- src/models/intel_pose.py
+++ src/models/intel_pose.py
@@ -6,7 +6,12 @@ from utils import Point, Joint, normalize
 class IntelPoseModel:
 
     def __init__(self):
-        pass
+        self.body_part_indexes = {
+            "L_hand": (10,),
+            "R_hand": (9,),
+            "L_foot": (16,),
+            "R_foot": (15,)
+        }
 
     @staticmethod
     def get_joints_from_result(result=None):
@@ -30,7 +35,43 @@ class IntelPoseModel:
             print("Unable to convert result to joints")
             raise
 
+    class Landmark(enum.IntEnum):
+        NOSE = 0
+        LEFT_EYE = 1
+        RIGHT_EYE = 2
+        LEFT_EAR = 3
+        RIGHT_EAR = 4
+        LEFT_SHOULDER = 5
+        RIGHT_SHOULDER = 6
+        LEFT_ELBOW = 7
+        RIGHT_ELBOW = 8
+        LEFT_WRIST = 9
+        RIGHT_WRIST = 10
+        LEFT_HIP = 11
+        RIGHT_HIP = 12
+        LEFT_KNEE = 13
+        RIGHT_KNEE = 14
+        LEFT_ANKLE = 15
+        RIGHT_ANKLE = 16
+
     SKELETON = frozenset([
-        (15, 13), (13, 11), (16, 14), (14, 12), (11, 12), (5, 11), (6, 12), (5, 6),
-        (5, 7), (6, 8), (7, 9), (8, 10), (1, 2), (0, 1), (0, 2), (1, 3), (2, 4), (3, 5), (4, 6)
+        (Landmark.LEFT_ANKLE, Landmark.LEFT_KNEE),
+        (Landmark.RIGHT_ANKLE, Landmark.RIGHT_KNEE),
+        (Landmark.LEFT_KNEE, Landmark.LEFT_HIP),
+        (Landmark.RIGHT_KNEE, Landmark.RIGHT_HIP),
+        (Landmark.LEFT_HIP, Landmark.RIGHT_HIP),
+        (Landmark.LEFT_SHOULDER, Landmark.LEFT_HIP),
+        (Landmark.RIGHT_SHOULDER, Landmark.RIGHT_HIP),
+        (Landmark.LEFT_SHOULDER, Landmark.RIGHT_HIP),
+        (Landmark.LEFT_SHOULDER, Landmark.LEFT_ELBOW),
+        (Landmark.RIGHT_SHOULDER, Landmark.RIGHT_ELBOW),
+        (Landmark.LEFT_ELBOW, Landmark.LEFT_WRIST),
+        (Landmark.RIGHT_ELBOW, Landmark.RIGHT_WRIST),
+        (Landmark.LEFT_EYE, Landmark.RIGHT_EYE),
+        (Landmark.NOSE, Landmark.LEFT_EYE),
+        (Landmark.NOSE, Landmark.RIGHT_EYE),
+        (Landmark.LEFT_EYE, Landmark.LEFT_EAR),
+        (Landmark.RIGHT_EYE, Landmark.RIGHT_EAR),
+        (Landmark.LEFT_EAR, Landmark.LEFT_SHOULDER),
+        (Landmark.RIGHT_EAR, Landmark.RIGHT_SHOULDER),
     ])
