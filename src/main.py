32bd4f7b08015d822d10a907f68066c17ca870dc
Dmitry Semenov <lonelyteapot@gmail.com>
2021-07-21 17:42:21 +0300

    Update main.py to test reader and display threads

--- src/main.py
+++ src/main.py
@@ -1,4 +1,28 @@
-import openvino_pose_detector
+import collections
+import time
+
+from video_display import VideoDisplay
+from video_reader import VideoReader
+
+
+def main():
+    frame_deque = collections.deque(maxlen=5)
+
+    video_reader = VideoReader(frame_deque)
+
+    fps = video_reader.benchmark_fps(num_frames=80)
+    video_display = VideoDisplay(frame_deque, fps=fps, window_name='Just Dance')
+
+    video_reader.start()
+    video_display.start()
+
+    time.sleep(15)
+    video_display.stop()
+    video_reader.stop()
+
+    video_display.join()
+    video_reader.join()
+
 
 if __name__ == '__main__':
-    openvino_pose_detector.main()
+    main()
