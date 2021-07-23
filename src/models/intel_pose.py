3196e1d88549a7f29fbc4a238f0161e5b81a4f76
Arapov Denis <ArapovZWERR@yandex.ru>
2021-07-23 06:05:07 +0300

    Extend wrist joints to match hands

--- src/models/intel_pose.py
+++ src/models/intel_pose.py
@@ -4,6 +4,29 @@ from models.base_pose import PoseModel
 from utils import Joint, normalize, log
 
 
+def distance(p1, p2):
+    return ((p1.x - p2.x)**2 + (p1.y - p2.y)**2)**(1/2)
+
+
+def get_sin(elbow_p, wrist_p, c):
+    return (wrist_p.y - elbow_p.y) / c
+
+
+def get_cos(elbow_p, wrist_p, c):
+    return (wrist_p.x - elbow_p.x) / c
+
+
+def get_additional_joint(divider, elbow_p, wrist_p):
+    cos_a = get_cos(elbow_p, wrist_p, divider)
+    sin_a = get_sin(elbow_p, wrist_p, divider)
+
+    a = sin_a / divider
+    b = cos_a / divider
+    if not (0 <= wrist_p.x + b <= 1 and 0 <= wrist_p.y + a <= 1):
+        return wrist_p
+    return Joint(wrist_p.x + b, wrist_p.y + a, wrist_p.score)
+
+
 class IntelPoseModel(PoseModel):
 
     @staticmethod
@@ -21,6 +44,10 @@ class IntelPoseModel(PoseModel):
                 if ind == 2:
                     break
                 joints.append([Joint(normalize(x, img_cols), normalize(y, img_rows), score) for x, y, score, _ in pose])
+                # Replace wrist on hand
+                joints[ind][9] = get_additional_joint(2, joints[ind][7], joints[ind][9])
+                joints[ind][10] = get_additional_joint(2, joints[ind][8], joints[ind][10])
+
             return joints
         except Exception:
             log.error("Unable to convert result to joints")
@@ -32,7 +59,7 @@ class IntelPoseModel(PoseModel):
         "L_foot": (15,),
         "R_foot": (16,)
     }
-
+# (7, 9) (8, 10)
     class Landmark(enum.IntEnum):
         NOSE = 0
         LEFT_EYE = 1
